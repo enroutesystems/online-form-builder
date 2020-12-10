63456dae7a7de34fcd44e9975de74432529539c5
jubaan <julio.ab@outlook.com>
2020-12-09 21:07:17 -0600

    Add base navbar component

---
+++ components/navbar/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import styles from '../../components/navbar';
+
+export default class Navbar extends Component {
+  render() {
+    return(
+      <p>hello world</p>
+    );
+  }
+}
+
