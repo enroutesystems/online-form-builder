dbab787f242094721a1058290d605042c022b75e
jubaan <julio.ab@outlook.com>
2020-12-10 10:17:41 -0600

    Add working navbar and global font dn reset styles

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -4,19 +4,21 @@ import styles from '../navbar/navbar.module.scss';
 export default class Navbar extends Component {
   render() {
     return(
-      <nav className={(`${styles["d-flex"]} ${styles.nav}`)}>
-        <a className={styles.brand} href=""><img className={styles.logo} src="" alt="" />LOGO</a>
-        <ul className={`${styles["d-flex"]} ${styles.menu}`}>
-          <li className={styles.item}>
-            <a className={styles.link, styles.btn} href="">Log In</a>
-          </li>
-          <li className={styles.item}>
-            <a className={styles.link, styles.btn} href="">Sign Up</a>
-          </li>
-          <li className={styles.item}>
-            <a className={styles.link} href=""><img className={styles.profile} src="/icons8-test-account-80.png" alt="user profile picture" /></a>
-          </li>
-        </ul>
+      <nav className={`${styles["d-flex"]} ${styles.nav}`}>
+        <div className={`${styles["d-flex"]}  ${styles.container}`}>
+          <a className={`${styles["d-flex"]} ${styles.brand} ${styles.link}`} href=""><img className={styles.logo} src="/icons8-poll-96.png" alt="app logo" />Poll</a>
+          <ul className={`${styles["d-flex"]} ${styles.menu}`}>
+            <li className={styles.item}>
+              <a className={`${styles.link} ${styles.btn} ${styles["log-in"]}`} href="">Log In</a>
+            </li>
+            <li className={styles.item}>
+              <a className={`${styles.link} ${styles.btn} ${styles["sign-up"]}`} href="">Sign Up</a>
+            </li>
+            <li className={styles.item}>
+              <a className={styles.link} href=""><img className={styles.profile} src="/icons8-test-account-80.png" alt="user profile picture" /></a>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
