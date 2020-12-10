085efc8a102f845d9c09b0686cecb419645eab2f
jubaan <julio.ab@outlook.com>
2020-12-09 22:28:00 -0600

    Styled nav version

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -4,7 +4,20 @@ import styles from '../navbar/navbar.module.scss';
 export default class Navbar extends Component {
   render() {
     return(
-      <p className={styles.nav}>hello world</p>
+      <nav className={(`${styles["d-flex"]} ${styles.nav}`)}>
+        <a className={styles.brand} href=""><img className={styles.logo} src="" alt="" />LOGO</a>
+        <ul className={`${styles["d-flex"]} ${styles.menu}`}>
+          <li className={styles.item}>
+            <a className={styles.link, styles.btn} href="">Log In</a>
+          </li>
+          <li className={styles.item}>
+            <a className={styles.link, styles.btn} href="">Sign Up</a>
+          </li>
+          <li className={styles.item}>
+            <a className={styles.link} href=""><img className={styles.profile} src="/icons8-test-account-80.png" alt="user profile picture" /></a>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
