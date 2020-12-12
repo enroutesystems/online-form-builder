d0ea36a0e456977f5569c48846a64d325fc79097
jubaan <julio.ab@outlook.com>
2020-12-11 18:19:30 -0600

    Add placeholder to textarea

--- components/textarea/index.js
+++ components/textarea/index.js
@@ -4,20 +4,11 @@ import styles from './textarea.module.scss';
 export default class Textarea extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: 'Type here your awesome answer...'
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
   }
 
   render() {
     return (
-      <textarea value={this.state.value} onChange={this.handleChange} />
+      <textarea onChange={this.handleChange} placeholder="Type something interesting..."/>
     );
   }
 }
