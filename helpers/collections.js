7f24b65741367faa7cbeaea8b0c4b84524c152bf
Manuel Rodriguez <marg.2197@gmail.com>
2020-12-13 15:22:52 -0600

    Form endpoints now can handle questions with multi option answers. helpers collection and questionType added: collections contains all the collections in our firestore database. questionTypes contains all the types of questions available to use in forms

---
+++ helpers/collections.js
@@ -0,0 +1,8 @@
+export default {
+    users: 'users',
+    userResponse: 'userResponse',
+    questions: 'questions',
+    optionAnswers: 'optionAnswers',
+    forms: 'forms',
+    allowedUsers: 'allowedUsers'
+}
