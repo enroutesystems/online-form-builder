918ffb2c573bb1dd5e7b6fae3f2330a41e77605c
jubaan <julio.ab@outlook.com>
2020-12-16 11:37:28 -0600

    Update fileUploader

--- components/fileUploader/index.js
+++ components/fileUploader/index.js
@@ -1,22 +1,13 @@
-import { storageRef } from "../../firebaseConfig";
 import { useState, useRef } from 'react';
 
-export default function FileUploader() {
+export default function FileUploader(props) {
   const uploader = useRef();
 
-  function uploadFile() {
-    const file = uploader.current.files[0];
-    const name = `${+new Date()}-${file.name}`;
-    const metadata = {
-      contentType: file.type
-    };
-    const task = storageRef.child(name).put(file, metadata);
+  function uploadFile(e) {
+    props.onFileUpload(e.target.files);
   }
 
   return (
-    <>
-      <input type="file" ref={uploader} name="uploadFile"/>
-      <button onClick={uploadFile}>upload to firebase</button>
-    </>
+      <input type="file" multiple onChange={uploadFile} ref={uploader} name="uploadFile"/>
   )
 }
