8c62f62d0bfda389d5a5e380ffdbd22f4772154b
Julio Añoveros <julio.ab@outlook.com>
2020-12-22 08:32:16 -0600

    Update navbar and homve page

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -12,31 +12,11 @@ export default function Navbar() {
           <img src="https://img.icons8.com/ios/50/000000/test.png"/>
           Formify
         </a>
-          {!session && (
-            <ul className={`${styles["d-flex"]} ${styles.menu}`}>
-              <li>
-                <a className={`${styles.btn} ${styles["log-in"]}`} href="/api/auth/signin">Sign In</a>
-              </li>
-              <li>
-                <a className={`${styles.btn} ${styles["sign-up"]}`} href="/api/auth/signup">Sign Up</a>
-              </li>
-            </ul>
-          )}
-          {session && (
-            <ul className={`${styles["d-flex"]} ${styles.menu}`}>
-              <li>
-                <a className={styles["d-flex"]} href="">
-                  {session.user.email}
-                  <img className={styles.profile}
-                       src="https://img.icons8.com/ios-filled/50/000000/user-male-circle.png"/>
-                </a>
-              </li>
-              <li>
-                <a className={styles["sign-out"]} href="/api/auth/signout"><img src="https://img.icons8.com/ios-filled/50/000000/logout-rounded.png"/></a>
-              </li>
-            </ul>
-
-          )}
+        <ul className={`${styles["d-flex"]} ${styles.menu}`}>
+          <li>
+            <a className={`${styles.btn} ${styles["log-in"]}`} href="/api/auth/signin">Log In / Register</a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
